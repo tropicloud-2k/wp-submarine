4826023ca9e89c584e135f1d93e33681407dbcdc
tropicloud <admin@tropicloud.net>
2015-06-30 13:15:03 -0300

    Tue Jun 30 13:15:03 BRT 2015

--- wps/bin/wps-setup.sh
+++ wps/bin/wps-setup.sh
@@ -5,7 +5,7 @@ wps_setup() {
 	# ---------------------------------------------------------------------------------
 
 	cat /wps/etc/init.d/nginx.ini | sed -e "s/example.com/$HOSTNAME/g" > $home/init.d/nginx.ini
-	cat /wps/etc/nginx/nginx.conf | sed -e "s/example.com/$HOSTNAME/g" > /etc/nginx/nginx.conf
+	cat /wps/etc/nginx/nginx_new.conf | sed -e "s/example.com/$HOSTNAME/g" > /etc/nginx/nginx.conf
 	
 	if [[  $WP_SSL == 'true'  ]];
 	then cat /wps/etc/nginx/wpssl.conf | sed -e "s/example.com/$HOSTNAME/g" > $home/conf.d/nginx.conf && wps_ssl
