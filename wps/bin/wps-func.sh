629cc7df9840d496a5939f570aef7e022d0e9996
tropicloud <admin@tropicloud.net>
2015-06-30 02:22:15 -0300

    Tue Jun 30 02:22:15 BRT 2015

--- wps/bin/wps-func.sh
+++ wps/bin/wps-func.sh
@@ -76,13 +76,13 @@ wps_version(){
 
 wps_chmod() { 
 
-	chown -R nginx:nginx /var/log/nginx && chmod -R 775 /var/log/nginx
-	chown -R nginx:nginx /var/lib/nginx && chmod -R 775 /var/lib/nginx
-	
 	chown -R $user:nginx $home
 		
 	find $home -type f -exec chmod 644 {} \;
 	find $home -type d -exec chmod 755 {} \;
+	
+	touch /var/log/nginx/error.log
+	chown $user:ngnx /var/log/nginx/error.log
 	
 }
 
