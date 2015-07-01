48bcdb52396c535c675646742c810c1c4e027916
tropicloud <admin@tropicloud.net>
2015-06-30 22:21:19 -0300

    Tue Jun 30 22:21:19 BRT 2015

--- wps/bin/wps-build.sh
+++ wps/bin/wps-build.sh
@@ -2,7 +2,7 @@
 # WPS BUILD
 # ---------------------------------------------------------------------------------	
 
-wps_build() { wps_header "Building image"
+wps_build() { wps_header "Build"
 
 
 	# PACKGES
@@ -41,54 +41,32 @@ wps_build() { wps_header "Building image"
 	rm -rf /var/cache/apk/*
 	rm -rf /var/lib/apt/lists/*
 
-	
 	# ADMINER
 	# -----------------------------------------------------------------------------	
 	
 	mkdir -p /usr/local/adminer
 	curl -sL http://www.adminer.org/latest-en.php > /usr/local/adminer/index.php
-
 	
 	# COMPOSER
 	# -----------------------------------------------------------------------------	
 
 	curl -sS https://getcomposer.org/installer | php
 	mv composer.phar /usr/local/bin/composer
-
 	
 	# PREDIS
 	# -----------------------------------------------------------------------------	
 	
 	pear channel-discover pear.nrk.io
 	pear install nrk/Predis
-
 	
 	# WP-CLI
 	# -----------------------------------------------------------------------------	
 
 	curl -sL https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar > /usr/local/bin/wp
 	chmod +x /usr/local/bin/wp
-
 	
-	# WP-USER
-	# -----------------------------------------------------------------------------	
-	
-	chmod +x /wps/wps.sh && ln -s /wps/wps.sh /usr/bin/wps
-	adduser -D -G nginx -s /bin/sh -u 1000 -h $home $user
-	echo "$user ALL = NOPASSWD : ALL" >> /etc/sudoers
-	
-	mkdir -p $home/conf.d
-	mkdir -p $home/init.d
-	mkdir -p $home/log/nginx
-	mkdir -p $home/log/php
-	mkdir -p $home/log/wps
-	mkdir -p $home/ssl
-	
-	cat /wps/etc/.profile > /root/.profile
-	cat /wps/etc/.profile > $home/.profile
-		
 	# -----------------------------------------------------------------------------	
 
-	wps_header "Build completed"
+	wps_header "Done!"
 }
 
