5dff5b1ed7fc9d42d7e821a0524eafa9c0cf07f0
tropicloud <admin@tropicloud.net>
2015-07-01 11:31:27 -0300

    Wed Jul  1 11:31:27 BRT 2015

--- wps/bin/wps-control.sh
+++ wps/bin/wps-control.sh
@@ -15,7 +15,7 @@ wps_start() {
 		else supervisorctl -u $user -p $WPS_PASSWORD -c $SUPERVISORD_CONF start $2
 		fi
 
-	else wps_chmod && exec su -l $user -c "supervisord -n -c $SUPERVISORD_CONF"
+	else wps_chmod && exec supervisord -n -c $SUPERVISORD_CONF
 	fi
 }
 
@@ -53,7 +53,7 @@ wps_restart() {
 		else supervisorctl -u $user -p $WPS_PASSWORD -c $SUPERVISORD_CONF restart $2
 		fi
 		
-	else wps_chmod && su -l $user -c "exec supervisord -n -c $SUPERVISORD_CONF"
+	else wps_chmod && exec supervisord -n -c $SUPERVISORD_CONF
 	fi
 	echo ""
 }
