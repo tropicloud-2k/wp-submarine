6d3784a55834a4b4469c5435b6e2999538cc0246
tropicloud <admin@tropicloud.net>
2015-07-01 15:49:02 -0300

    Wed Jul  1 15:49:02 BRT 2015

--- wps/bin/wps-mysql.sh
+++ wps/bin/wps-mysql.sh
@@ -4,13 +4,17 @@
 
 wps_mysql_create() {
 	mysql -u root -p$MYSQL_ENV_MYSQL_ROOT_PASSWORD -h $DB_HOST -e "CREATE DATABASE IF NOT EXISTS $DB_NAME"
-	mysql -u root -p$MYSQL_ENV_MYSQL_ROOT_PASSWORD -h $DB_HOST -e "GRANT ALL PRIVILEGES ON '$DB_NAME'.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD'"
+# 	mysql -u root -p$MYSQL_ENV_MYSQL_ROOT_PASSWORD -h $DB_HOST -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD'"
+# 	mysql -u root -p$MYSQL_ENV_MYSQL_ROOT_PASSWORD -h $DB_HOST -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' WITH GRANT OPTION"
+ 	mysql -u root -p$MYSQL_ENV_MYSQL_ROOT_PASSWORD -h $DB_HOST -e "grant all on `$DB_NAME`.* to '$DB_USER'@'%' identified by '$DB_PASSWORD';"
 	mysql -u root -p$MYSQL_ENV_MYSQL_ROOT_PASSWORD -h $DB_HOST -e "FLUSH PRIVILEGES"
 }
 
 wps_mariadb_create() {
 	mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME"
-	mysql -u root -e "GRANT ALL PRIVILEGES ON '$DB_NAME'.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD'"
+# 	mysql -u root -e "CREATE USER '$user'@'%' IDENTIFIED BY '$DB_PASSWORD'"
+# 	mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '$user'@'%' WITH GRANT OPTION"
+ 	mysql -u root -e "grant all on `$DB_NAME`.* to '$DB_USER'@'%' identified by '$DB_PASSWORD';"
 	mysql -u root -e "DELETE FROM mysql.user WHERE User='';"
 	mysql -u root -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
 	mysql -u root -e "DROP DATABASE test;"
@@ -36,15 +40,17 @@ wps_mysql_setup() {
 	mysqld_safe > /dev/null 2>&1 &
 	
 	echo -ne "Starting mysql server..."
-	while [[  ! -e /run/mysqld/mysqld.sock  ]]; do 
-		echo -n '.' && sleep 1;
-	done && echo -ne " done.\n"
+	while [[  ! -e /run/mysqld/mysqld.sock  ]]; do echo -n '.' && sleep 1; done && echo -ne " done.\n"
 	
 	echo -ne "Creating mysql database..."
-	while ! wps_mariadb_create true; do 
-		echo -n '.' && sleep 1; 
-	done && echo -ne " done.\n"
-
+	while ! wps_mariadb_create true; do echo -n '.' && sleep 1; done && echo -ne " done.\n"
+	
+	echo -ne "Securing mysql installation..."
+	while ! wps_mariadb_secure true; do echo -n '.' && sleep 1; done && echo -ne " done.\n"
+	
+	echo -ne "Flushing mysql privileges..."
+	while ! `mysql -u root -e "FLUSH PRIVILEGES"` true; do echo -n '.' && sleep 1; done && echo -ne " done.\n"
+	
 	mysqladmin -u root shutdown
 	
 	# -----------------------------------------------------------------------------	
