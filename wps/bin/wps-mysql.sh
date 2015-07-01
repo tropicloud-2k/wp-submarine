f2ba4d473259489a75d7f2330bce7c23c0f53cda
tropicloud <admin@tropicloud.net>
2015-07-01 15:37:52 -0300

    Wed Jul  1 15:37:52 BRT 2015

--- wps/bin/wps-mysql.sh
+++ wps/bin/wps-mysql.sh
@@ -4,18 +4,17 @@
 
 wps_mysql_create() {
 	mysql -u root -p$MYSQL_ENV_MYSQL_ROOT_PASSWORD -h $DB_HOST -e "CREATE DATABASE IF NOT EXISTS $DB_NAME"
-	mysql -u root -p$MYSQL_ENV_MYSQL_ROOT_PASSWORD -h $DB_HOST -e "GRANT ALL ON '$DB_NAME'.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD'"
+	mysql -u root -p$MYSQL_ENV_MYSQL_ROOT_PASSWORD -h $DB_HOST -e "GRANT ALL PRIVILEGES ON '$DB_NAME'.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD'"
 	mysql -u root -p$MYSQL_ENV_MYSQL_ROOT_PASSWORD -h $DB_HOST -e "FLUSH PRIVILEGES"
 }
 
 wps_mariadb_create() {
 	mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME"
-	mysql -u root -e "GRANT ALL ON '$DB_NAME'.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD'"
+	mysql -u root -e "GRANT ALL PRIVILEGES ON '$DB_NAME'.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD'"
 	mysql -u root -e "DELETE FROM mysql.user WHERE User='';"
 	mysql -u root -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
 	mysql -u root -e "DROP DATABASE test;"
 	mysql -u root -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%'"
-	mysql -u root -e "FLUSH PRIVILEGES"
 }
 
 
@@ -36,11 +35,16 @@ wps_mysql_setup() {
 	mysql_install_db --user=mysql > /dev/null 2>&1
 	mysqld_safe > /dev/null 2>&1 &
 	
-	echo -ne "Configuring MySQL server..."
-	while ! wps_mariadb_create true; do 
+	echo -ne "Starting mysql server..."
+	while [[  ! -e /run/mysqld/mysqld.sock  ]]; do 
 		echo -n '.' && sleep 1;
 	done && echo -ne " done.\n"
 	
+	echo -ne "Creating mysql database..."
+	while ! wps_mariadb_create true; do 
+		echo -n '.' && sleep 1; 
+	done && echo -ne " done.\n"
+
 	mysqladmin -u root shutdown
 	
 	# -----------------------------------------------------------------------------	
