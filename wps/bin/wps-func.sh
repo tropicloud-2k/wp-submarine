461a9807946a0bd0310099b3574df979c793b25c
tropicloud <admin@tropicloud.net>
2015-07-01 21:05:28 -0300

    Wed Jul  1 21:05:28 BRT 2015

--- wps/bin/wps-func.sh
+++ wps/bin/wps-func.sh
@@ -51,17 +51,17 @@ wps_header() {
 
 wps_links() {
 
-	if [[  -n $MYSQL_PORT  ]];
+	if [[  ! -z $MYSQL_PORT  ]];
 	then echo -e "\033[1;32m  •\033[0;37m MySQL\033[0m -> `echo $WPS_MYSQL`"
 	else echo -e "\033[1;31m  •\033[0;37m MySQL\033[0m (not linked)"
 	fi	
 
-	if [[  -n $REDIS_PORT  ]];
+	if [[  ! -z $REDIS_PORT  ]];
 	then echo -e "\033[1;32m  •\033[0;37m Redis\033[0m -> `echo $WPS_REDIS`"		
 	else echo -e "\033[1;31m  •\033[0;37m Redis\033[0m (not linked)"
 	fi		
 
-	if [[  -n $MEMCACHED_PORT  ]];
+	if [[  ! -z $MEMCACHED_PORT  ]];
 	then echo -e "\033[1;32m  •\033[0;37m Memcached\033[0m -> `echo $WPS_MEMCACHED`"
 	else echo -e "\033[1;31m  •\033[0;37m Memcached\033[0m (not linked)"
 	fi
--- wps/bin/wps-mysql.sh
+++ wps/bin/wps-mysql.sh
@@ -3,12 +3,10 @@
 # ---------------------------------------------------------------------------------
 
 wps_mysql_create() {
-	MYSQL="mysql -u root -p$MYSQL_ENV_MYSQL_ROOT_PASSWORD -h $DB_HOST"
-	while ! `$MYSQL -e "SHOW STATUS"` true; do sleep 1; done
-	$MYSQL -e "CREATE DATABASE IF NOT EXISTS $DB_NAME"
- 	$MYSQL -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD'"
- 	$MYSQL -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' WITH GRANT OPTION"
-	$MYSQL -e "FLUSH PRIVILEGES"
+	mysql -u root -p$MYSQL_ENV_MYSQL_ROOT_PASSWORD -h $DB_HOST -e "CREATE DATABASE IF NOT EXISTS $DB_NAME"
+ 	mysql -u root -p$MYSQL_ENV_MYSQL_ROOT_PASSWORD -h $DB_HOST -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD'"
+ 	mysql -u root -p$MYSQL_ENV_MYSQL_ROOT_PASSWORD -h $DB_HOST -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' WITH GRANT OPTION"
+	mysql -u root -p$MYSQL_ENV_MYSQL_ROOT_PASSWORD -h $DB_HOST -e "FLUSH PRIVILEGES"
 }
 
 wps_mariadb_create() {
